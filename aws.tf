089d74950cdcc8ea219841876b1b78fcfbfc6050
umarzp <mukthar.zp@gmail.com>
2024-01-23 22:30:45 +0530

    first commit

---
+++ aws.tf
@@ -0,0 +1,19 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  default     = "AKIASVCVN2RKVJWDOBWT"
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  default     = "+QCTrJbfNPnUYWpe0vARr7VXkQWGd8sZWoTcdMUB"
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+
