b99fed8f0e25afc03786d04545b3a53fb78d1ad3
umarzp <mukthar.zp@gmail.com>
2024-01-23 22:39:00 +0530

    first commit

---
+++ provider.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
+
+}
+
+resource "aws_iam_user" "suhail" {
+  name = "suhail"
+
+}
